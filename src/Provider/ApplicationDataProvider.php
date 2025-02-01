a548e5301736b575e46dc1b180fb7a20e5711ca7
devToms <tomaszkedziora14@example.com>
2025-02-01 22:34:02 +0100

    fix paths to vendor and var

--- src/Provider/ApplicationDataProvider.php
+++ src/Provider/ApplicationDataProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Provider;
 
+use Psr\Log\LoggerInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Repository\ApplicationRepositoryInterface;
@@ -9,15 +10,17 @@ use App\Repository\ApplicationRepositoryInterface;
 final class ApplicationDataProvider implements ProviderInterface
 {
     public function __construct(
-        private readonly ApplicationRepositoryInterface $applicationRepository
+        private readonly ApplicationRepositoryInterface $applicationRepository,
+        private readonly LoggerInterface $logger
+
     ) {}
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): iterable
     {
         $options = $operation->getOptions();
         $readStatus = $options['read_status'] ?? false;
-        $queryBuilder = $this->applicationRepository->findByReadStatusAndOrder($readStatus);
-
+        $queryBuilder = $this->applicationRepository->findByReadStatusAndOrder(false);
+        $this->logger->info('Query Result', ['data' => $queryBuilder->getQuery()->getResult()]);
         return $queryBuilder->getQuery()->getResult();
     }
 }
